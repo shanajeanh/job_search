9754454abe12c8749759812404382505f627b36d
Shana <shanajeanhausman@gmail.com>
2017-09-08 19:21:25 -0400

    start test creation

---
+++ spec/features/user_creates_account_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'user can create an account through github' do
+
+  scenario 'logged out user can see a log in button' do
+
+  end
+
+  scenario 'logged in user can see a log out button' do
+
+  end
+
+  scenario 'logged in user can see buttons for events, applications, projects' do
+
+  end
+
+  scenario 'users account is connected through github' do
+    
+  end
+
+end
